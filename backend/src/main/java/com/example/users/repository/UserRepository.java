ae9cb4dd6d582dc28b8877c2bb54a03ff609615f
Gabriel Alves Varella da Costa <gabriel.varella1327@hotmail.com>
2025-10-08 19:17:20 -0300

    style: removendo metodos não utilizados.

--- backend/src/main/java/com/example/users/repository/UserRepository.java
+++ backend/src/main/java/com/example/users/repository/UserRepository.java
@@ -5,11 +5,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
-import java.util.Optional;
+
 
 public interface UserRepository extends JpaRepository<User, Long> {
 
-    // Optional<User> findByEmail(String email);
 
     List<User> findByRole(String role);
 
